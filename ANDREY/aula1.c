567a9bf1ff65fa88dccfe86ae635d3ae19cb28c9
Andrey-Cavalcante <127567756+Andrey-Cavalcante@users.noreply.github.com>
2023-08-08 20:44:07 -0400

    Add files via upload

---
+++ ANDREY/aula1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+/*char nome[100];
+int idade, ano, atual;
+
+printf("Escreva o seu nome: ");
+gets(nome);
+
+printf("Qual ano voce nasceu: ");
+scanf("%d",&ano);
+
+printf("Qual ano voce esta: ");
+scanf("%d",&atual);
+
+idade = atual - ano;
+
+printf("Ola %s, Voce tem %d de idade", nome,idade)
+-------------------------------------------------------*/
+/*float n1, n2, soma, mult, div;
+
+
+printf("Informe o numero 1: ");
+scanf("%f",&n1);
+
+printf("Informe o numero 2: ");
+scanf("%f",&n2);
+
+soma = n1 + n2;
+mult = n1 * n2;
+div = n1 / n2;
+
+printf("\nO valor da soma e: %2.f", soma);
+printf("\nO valor da mult e: %2.f", mult);
+printf("\nO valor da divisao e: %2.f", div);
+------------------------------------------------------*/
+
+return 0;
+
+}
+
+
---
+++ ANDREY/list1/ex1.cpp
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+/*1)ATV	
+ 
+  printf("Linguagem de programacao C++");
+-------------------------------------------*/
+/*2)ATV
+int nu1, soma;
+
+printf("Informe um numero para ser dobrado: ");
+scanf("%d", &nu1);
+
+  
+soma = nu1*2;
+
+printf("O dobre desse numero é: %d", soma);
+-------------------------------------------*/
+/*3)ATV
+int nu1, nu2;
+
+printf("Informe um valor: ");
+scanf("%d",&nu1);	
+		
+printf("Informe um valor: ");
+scanf("%d",&nu2);
+
+printf("Os numeros digitados sao: %d e %d", nu1, nu2);		
+------------------------------------------------------*/
+/*4)ATV
+int nu1, nu2, soma;
+
+printf("Informe um valor: ");
+scanf("%d",&nu1);	
+		
+printf("Informe um valor: ");
+scanf("%d",&nu2);
+
+soma = nu1+nu2;
+
+printf("A soma dos valores sao: %d", soma);
+--------------------------------------------*/
+/*5)ATV
+int n1, resto;
+
+printf("Informe um numero: ");
+scanf("%d", &n1);
+
+printf("O resto da divisao e %d ",n1%3);
+----------------------------------------*/
+/*6)ATV
+int n1;
+
+printf("Informe um valor: ");
+scanf("%d",&n1);
+
+printf("O quonciente e: %d",n1/3);
+----------------------------------*/
+/*7)ATV
+int nu1, nu2, A, B;
+
+printf("Informe um valor A: ");
+scanf("%d",&nu1);	
+		
+printf("Informe um valor B: ");
+scanf("%d",&nu2);
+
+printf("Os valores sao: %d e %d", nu2, nu1);
+--------------------------------------------*/
+/*8)
+int hora, valor, bruto;
+
+printf("Informe a quantidade de horas trabalhadas: ");
+scanf("%d",&hora);
+
+printf("Valor de hora/trabalho: ");
+scanf("%d",&valor);
+
+bruto = hora*valor;
+
+printf("O salario bruto e %d",bruto);
+------------------------------------*/
+		
+return 0;
+}
